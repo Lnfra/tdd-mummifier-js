62963d2cc6ebebd2cac14e60a56a2c351bae94f0
Angeline <atan@thoughtworks.com>
2019-06-06 16:35:11 +0800

    Refactor all tests to use helper to reduce duplication

--- StringMummifier.test.js
+++ StringMummifier.test.js
@@ -2,22 +2,23 @@ const StringMummifier = require("./StringMummifier");
 
 describe("String Mummifier", () => {
   it("should not mummify empty string", () => {
-    const mummifier = new StringMummifier();
-    expect(mummifier.transform("")).toEqual("");
+    checkMummified("", "");
   });
 
   it("should not mummify word with no vowels", () => {
-    const mummifier = new StringMummifier();
-    expect(mummifier.transform("str")).toEqual("str");
+    checkMummified("str", "str");
   });
 
   it("should mummify a vowel", () => {
-    const mummifier = new StringMummifier();
-    expect(mummifier.transform("a")).toEqual("mummy");
+    checkMummified("a", "mummy");
   });
 
   it("should mummify consonants and a vowel", () => {
-    const mummifier = new StringMummifier();
-    expect(mummifier.transform("bla")).toEqual("blmummy");
+    checkMummified("bla", "blmummy");
   });
+
+  function checkMummified(input, expected) {
+    const mummifier = new StringMummifier();
+    expect(mummifier.transform(input)).toEqual(expected);
+  }
 });
