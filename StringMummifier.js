5605dfaaa4cdfd233f00861ddf5cfaa65e9d39dc
Angeline <atan@thoughtworks.com>
2019-06-06 15:29:05 +0800

    Add code to pass test: should mummify one vowel

--- StringMummifier.js
+++ StringMummifier.js
@@ -1,5 +1,10 @@
 class StringMummifier {
   transform(word) {
+    for (let letter of word) {
+      if ("aeiou".includes(letter)) {
+        return "mummy";
+      }
+    }
     return word;
   }
 }
