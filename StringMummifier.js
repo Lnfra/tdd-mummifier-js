e8d04c8b2334b8e13b78b66be8216ad5b38ab302
Angeline <atan@thoughtworks.com>
2019-06-06 19:02:23 +0800

    Add code: should mummify continuous vowels only once

--- StringMummifier.js
+++ StringMummifier.js
@@ -8,13 +8,16 @@ class StringMummifier {
   }
 
   replaceVowels(word) {
-    const letterArray = word.split("");
-    return letterArray
-      .map(letter => {
-        if (!this.isVowel(letter)) return letter;
-        return REPLACEMENT;
-      })
-      .join("");
+    let mummified = "";
+
+    for (let letter of word) {
+      if (!this.isVowel(letter)){
+        mummified += letter;
+      } else if (!mummified.endsWith(REPLACEMENT)) {
+        mummified += REPLACEMENT;
+      }
+    }
+    return mummified;
   }
 
   isVowelLessTan30Percent(word) {
